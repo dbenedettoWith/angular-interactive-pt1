a4a07f96239224bb94839f9416c52695a102f0e3
dbenedettoWith <118215649+dbenedettoWith@users.noreply.github.com>
2022-11-14 15:51:47 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+  ],
+  declarations: [AppComponent, HelloComponent, TopBarComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
